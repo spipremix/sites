ad20cb476ffd50eee3f52ad07460053a48031938
Matthieu Marcillaud <marcimat@rezo.net>
2010-09-25 08:22:37 +0000

    Déplacement des déclarations de bases, autorisations, taches périodiques des disparus, boucles et recherche dans le plugin sites. + déplacement de formulaires/ à la racine
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/sites@41117 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sites_autoriser.php
+++ sites_autoriser.php
@@ -27,4 +27,26 @@ function autoriser_controle_syndic_bouton_dist($faire, $type, $id, $qui, $opt){
 	return 	(sql_countsel('spip_syndic_articles'));
 }
 
+
+
+
+// Autoriser a modifier un site
+// http://doc.spip.org/@autoriser_site_modifier_dist
+function autoriser_site_modifier_dist($faire, $type, $id, $qui, $opt) {
+	if ($qui['statut'] == '0minirezo' AND !$qui['restreint'])
+		return true;
+
+	$t = sql_fetsel("id_rubrique,statut", "spip_syndic", "id_syndic=".sql_quote($id));
+	return ($t
+		AND autoriser('voir','rubrique',$t['id_rubrique'])
+		AND ($t['statut'] == 'prop'
+			OR autoriser('modifier', 'rubrique', $t['id_rubrique'])
+		)
+	);
+}
+// Autoriser a voir un site $id_syndic
+// http://doc.spip.org/@autoriser_site_voir_dist
+function autoriser_site_voir_dist($faire, $type, $id, $qui, $opt) {
+	return autoriser_site_modifier_dist($faire, $type, $id, $qui, $opt);
+}
 ?>
--- sites_pipelines.php
+++ sites_pipelines.php
@@ -10,6 +10,21 @@
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
+/**
+ * Definir les meta de configuration liee aux syndications et sites
+ *
+ * @param array $metas
+ * @return array
+ */
+function sites_configurer_liste_metas($metas){
+	$metas['activer_sites']    = 'non';
+	$metas['proposer_sites']   = 0;
+	$metas['activer_syndic']   = 'oui';
+	$metas['moderation_sites'] = 'non';
+	return $metas;
+}
+
+
 
 /**
  * Permet des calculs de noms d'url sur les sites. 
@@ -38,6 +53,52 @@ function sites_taches_generales_cron($taches_generales){
 	}
 		
 	return $taches_generales;
+}
+
+
+/**
+ * Optimiser la base de donnee en supprimant les liens orphelins
+ *
+ * @param int $n
+ * @return int
+ */
+function sites_optimiser_base_disparus($flux){
+	$n = &$flux['data'];
+
+
+
+	sql_delete("spip_syndic", "maj < $mydate AND statut = 'refuse'");
+
+
+	# les articles syndiques appartenant a des sites effaces
+	$res = sql_select("S.id_syndic AS id",
+		      "spip_syndic_articles AS S
+		        LEFT JOIN spip_syndic AS syndic
+		          ON S.id_syndic=syndic.id_syndic",
+			"syndic.id_syndic IS NULL");
+
+	$n+= optimiser_sansref('spip_syndic_articles', 'id_syndic', $res);
+	
+
+	return $flux;
+
+}
+
+
+
+
+/**
+ * Definir la liste des champs de recherche sur la table syndic_articles 
+ *
+ * @param array $liste
+ * @return array
+ */
+function sites_rechercher_liste_des_champs($liste){
+	$liste['syndic_article'] = array(
+	  'titre' => 5, 'descriptif' => 1
+	);
+
+	return $liste;
 }
 
 ?>
