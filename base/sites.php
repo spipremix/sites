416ab4257a434c78df4dfdfa1d021a238ab9ccc0
Matthieu Marcillaud <marcimat@rezo.net>
2010-09-25 08:22:37 +0000

    Déplacement des déclarations de bases, autorisations, taches périodiques des disparus, boucles et recherche dans le plugin sites. + déplacement de formulaires/ à la racine

---
+++ base/sites.php
@@ -0,0 +1,139 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+/**
+ * Interfaces des tables syndic et syndic article
+ *
+ * @param array $interfaces
+ * @return array
+ */
+function sites_declarer_tables_interfaces($interfaces){
+
+	$interfaces['table_des_tables']['syndication']='syndic';
+	$interfaces['table_des_tables']['syndic']='syndic';
+	$interfaces['table_des_tables']['syndic_articles']='syndic_articles';
+
+	# ne sert plus ? verifier balise_URL_ARTICLE
+	$interfaces['exceptions_des_tables']['syndic_articles']['url_article']='url';
+	# ne sert plus ? verifier balise_LESAUTEURS
+	$interfaces['exceptions_des_tables']['syndic_articles']['lesauteurs']='lesauteurs';
+	$interfaces['exceptions_des_tables']['syndic_articles']['url_site']=array('syndic', 'url_site');
+	$interfaces['exceptions_des_tables']['syndic_articles']['nom_site']=array('syndic', 'nom_site');
+
+	$interfaces['table_date']['syndication'] = 'date';
+	$interfaces['table_date']['syndic_articles'] = 'date';
+
+	$interfaces['table_titre']['site'] = "nom_site AS titre, '' AS lang";
+	$interfaces['table_titre']['syndic'] = "nom_site AS titre, '' AS lang";
+
+	$interfaces['tables_jointures']['spip_syndic_articles'][]= 'syndic';
+
+	$interfaces['table_des_traitements']['NOM_SITE'][]=  _TRAITEMENT_TYPO;
+
+	// Articles syndiques : passage des donnees telles quelles, sans traitement typo
+	// A noter, dans applique_filtres la securite et conformite XHTML de ces champs
+	// est assuree par safehtml()
+	foreach(array('TITRE','DESCRIPTIF','SOURCE') as $balise)
+		if (!isset($table_des_traitements[$balise]['syndic_articles']))
+			$table_des_traitements[$balise]['syndic_articles'] = '%s';
+
+	return $interfaces;
+}
+
+
+/**
+ * Table principale spip_syndic et spip_syndic_articles 
+ *
+ * @param array $tables_principales
+ * @return array
+ */
+function sites_declarer_tables_principales($tables_principales){
+
+
+	$spip_syndic = array(
+			"id_syndic"	=> "bigint(21) NOT NULL",
+			"id_rubrique"	=> "bigint(21) DEFAULT '0' NOT NULL",
+			"id_secteur"	=> "bigint(21) DEFAULT '0' NOT NULL",
+			"nom_site"	=> "text DEFAULT '' NOT NULL",
+			"url_site"	=> "text DEFAULT '' NOT NULL",
+			"url_syndic"	=> "text DEFAULT '' NOT NULL",
+			"descriptif"	=> "text DEFAULT '' NOT NULL",
+			"maj"	=> "TIMESTAMP",
+			"syndication"	=> "VARCHAR(3) DEFAULT '' NOT NULL",
+			"statut"	=> "varchar(10) DEFAULT '0' NOT NULL",
+			"date"	=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			"date_syndic"	=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			"date_index"	=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			"moderation"	=> "VARCHAR(3) DEFAULT 'non'",
+			"miroir"	=> "VARCHAR(3) DEFAULT 'non'",
+			"oubli"	=> "VARCHAR(3) DEFAULT 'non'",
+			"resume"	=> "VARCHAR(3) DEFAULT 'oui'"
+	);
+
+	$spip_syndic_key = array(
+			"PRIMARY KEY"	=> "id_syndic",
+			"KEY id_rubrique"	=> "id_rubrique",
+			"KEY id_secteur"	=> "id_secteur",
+			"KEY statut"	=> "statut, date_syndic",
+	);
+	$spip_syndic_join = array(
+			"id_syndic"=>"id_syndic",
+			"id_rubrique"=>"id_rubrique");
+			
+	$spip_syndic_articles = array(
+			"id_syndic_article"	=> "bigint(21) NOT NULL",
+			"id_syndic"	=> "bigint(21) DEFAULT '0' NOT NULL",
+			"titre"	=> "text DEFAULT '' NOT NULL",
+			"url"	=> "VARCHAR(255) DEFAULT '' NOT NULL",
+			"date"	=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			"lesauteurs"	=> "text DEFAULT '' NOT NULL",
+			"maj"	=> "TIMESTAMP",
+			"statut"	=> "varchar(10) DEFAULT '0' NOT NULL",
+			"descriptif"	=> "text DEFAULT '' NOT NULL",
+			"lang"	=> "VARCHAR(10) DEFAULT '' NOT NULL",
+			"url_source" => "TINYTEXT DEFAULT '' NOT NULL",
+			"source" => "TINYTEXT DEFAULT '' NOT NULL",
+			"tags" => "TEXT DEFAULT '' NOT NULL");
+
+	$spip_syndic_articles_key = array(
+			"PRIMARY KEY"	=> "id_syndic_article",
+			"KEY id_syndic"	=> "id_syndic",
+			"KEY statut"	=> "statut",
+			"KEY url"	=> "url");
+	$spip_syndic_articles_join = array(
+			"id_syndic_article"=>"id_syndic_article",
+			"id_syndic"=>"id_syndic");
+
+	$tables_principales['spip_syndic'] =
+		array('field' => &$spip_syndic, 'key' => &$spip_syndic_key, 'join' => &$spip_syndic_join);
+	$tables_principales['spip_syndic_articles']	=
+		array('field' => &$spip_syndic_articles, 'key' => &$spip_syndic_articles_key, 'join' => &$spip_syndic_articles_join);
+	
+
+	return $tables_principales;
+}
+
+
+/**
+ * Declarer le surnom des syndic
+ *
+ * @param array $table
+ * @return array
+ */
+function sites_declarer_tables_objets_surnoms($table){
+	$table['syndic'] = 'syndic'; 
+	$table['site'] = 'syndic';  # hum hum
+	$table['syndic_article'] = 'syndic_article'; 
+	return $table;
+}
+
+?>
---
+++ base/sites_upgrade.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+/**
+ * Installation/maj des tables syndic et syndic articles
+ *
+ * @param string $nom_meta_base_version
+ * @param string $version_cible
+ */
+function sites_upgrade($nom_meta_base_version,$version_cible){
+	$current_version = 0.0;
+	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+
+		if ($current_version==0.0){
+			include_spip('base/create');
+			// creer les tables
+			creer_base();
+			// mettre les metas par defaut
+			$config = charger_fonction('config','inc');
+			$config();
+			ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
+		}
+	}
+}
+
+
+/**
+ * Desinstallation/suppression des tables mots et groupes de mots
+ *
+ * @param string $nom_meta_base_version
+ */
+function sites_vider_tables($nom_meta_base_version) {
+	sql_drop_table("spip_syndic");
+	sql_drop_table("spip_syndic_articles");
+
+	effacer_meta($nom_meta_base_version);
+}
+
+?>
