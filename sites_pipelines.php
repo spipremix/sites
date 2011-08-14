f3a0ea4cb016f6c44bd6abaa144a3df444539a96
Cerdic <cedric@yterium.com>
2011-08-14 11:06:52 +0000

    http://core.spip.org/issues/2214 decompte du nombre d'elements dans les rubriques : c'est de la charge de chaque extension
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/sites@50463 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- sites_pipelines.php
+++ sites_pipelines.php
@@ -234,5 +234,22 @@ function sites_trig_propager_les_secteurs($flux){
 	return $flux;
 }
 
+/**
+ * Afficher le nombre de sites dans chaque rubrique
+ *
+ * @param array $flux
+ * @return array
+ */
+function sites_boite_infos($flux){
+	if ($flux['args']['type']=='rubrique'
+	  AND $id_rubrique = $flux['args']['id']){
+		if ($nb = sql_countsel('spip_syndic',"statut='publie' AND id_rubrique=".intval($id_rubrique))){
+			$nb = "<div>". singulier_ou_pluriel($nb, "info_1_site", "info_nb_sites") . "</div>";
+			if ($p = strpos($flux['data'],"<!--nb_elements-->"))
+				$flux['data'] = substr_replace($flux['data'],$nb,$p,0);
+		}
+	}
+	return $flux;
+}
 
 ?>
