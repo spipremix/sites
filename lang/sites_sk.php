0f8b6d503954de48ebabc03d2a359d557825d6d0
salvatore <salvatore@rezo.net>
2012-01-03 11:04:08 +0000

    langues (sites)

--- lang/sites_sk.php
+++ lang/sites_sk.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'articles_dispo' => 'Čaká',
+	'articles_dispo' => 'Čakajúce',
 	'articles_meme_auteur' => 'Všetky články tohto autora',
 	'articles_off' => 'Bloky',
 	'articles_publie' => 'Publikované',
@@ -16,15 +16,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'articles_tous' => 'Všetky',
 	'aucun_article_syndic' => 'Žiaden syndikovaný článok',
 	'avis_echec_syndication_01' => 'Syndikácia zlyhala: buď sa vybrané backendové rozhranie nedá čítať, alebo neobsahuje žiadne články.',
-	'avis_echec_syndication_02' => 'Syndication failed: could not reach the backend of this site.',
+	'avis_echec_syndication_02' => 'Syndikácia sa nepodarila: nepodarilo sa nájsť backendový súbor tejto stránky.',
 	'avis_site_introuvable' => 'Stránka sa nenašla',
 	'avis_site_syndique_probleme' => 'Upozornenie: pri syndikácii tejto stránky sa vyskytol problém; systém je dočasne narušený. Skontrolujte adresu syndikačného súboru tejto stránky (<b>@url_syndic@</b>) a skúste získať informácie znova.',
-	'avis_sites_probleme_syndication' => 'These sites encountered a syndication problem',
-	'avis_sites_syndiques_probleme' => 'These syndicated sites generated a problem',
+	'avis_sites_probleme_syndication' => 'Pri týchto stránkach sa vyskytol problém so syndikáciou',
+	'avis_sites_syndiques_probleme' => 'Pri týchto syndikovaných stránkach sa vyskytol problém',
 
 	// B
-	'bouton_exporter' => 'Exporter', # NEW
-	'bouton_importer' => 'Importer', # NEW
+	'bouton_exporter' => 'Exportovať',
+	'bouton_importer' => 'Nahrať',
 	'bouton_radio_modere_posteriori' => 'Kontrola po publikovaní',
 	'bouton_radio_modere_priori' => 'Kontrola pred publikovaním',
 	'bouton_radio_non_syndication' => 'Žiadna syndikácia',
@@ -37,8 +37,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'entree_adresse_fichier_syndication' => 'Adresa syndikačného súboru:',
 	'entree_adresse_site' => '<b>Adresa stránky</b> [Povinné]',
 	'entree_description_site' => 'Popis stránky',
-	'erreur_fichier_format_inconnu' => 'Le format du fichier @fichier@ n\'est pas pris en charge.', # NEW
-	'erreur_fichier_incorrect' => 'Impossible de lire le fichier.', # NEW
+	'erreur_fichier_format_inconnu' => 'Formát súboru @fichier@ nie je podporovaný.',
+	'erreur_fichier_incorrect' => 'Súbor sa nedá čítať.',
 
 	// F
 	'form_prop_nom_site' => 'Názov stránky',
@@ -55,50 +55,50 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_valider_article' => 'Schváliť tento článok',
 	'icone_valider_articles' => 'Schváliť tieto články',
 	'icone_voir_sites_references' => 'Zobraziť linky',
-	'info_1_site_importe' => '1 site a été importé', # NEW
+	'info_1_site_importe' => '1 stránka sa nahrala',
 	'info_a_valider' => '[čaká na schválenie]',
-	'info_aucun_site_importe' => 'Aucun site n\'a pu être importé', # NEW
+	'info_aucun_site_importe' => 'Nenahrala sa žiadna stránka',
 	'info_bloquer' => 'zablokovať',
 	'info_bloquer_lien' => 'Zablokovať tento odkaz',
-	'info_derniere_syndication' => 'The last syndication of this site was carried out on',
-	'info_liens_syndiques_1' => 'syndicated links',
+	'info_derniere_syndication' => 'Posledná syndikácia bola vykonaná',
+	'info_liens_syndiques_1' => 'syndikované odkazy',
 	'info_liens_syndiques_2' => 'čaká na schválenie.',
-	'info_nb_sites_importes' => '@nb@ sites ont été importés', # NEW
+	'info_nb_sites_importes' => '@nb@ stránok sa nahralo',
 	'info_nom_site_2' => '<b>Názov stránky</b> [Povinné]',
-	'info_panne_site_syndique' => 'Syndicated site out of order',
+	'info_panne_site_syndique' => 'Syndikovaná stránka mimo prevádzky',
 	'info_probleme_grave' => 'chyba u',
-	'info_question_proposer_site' => 'Who can propose referenced sites?',
+	'info_question_proposer_site' => 'Kto môže navrhovať stránky, na ktoré sa odkazuje?',
 	'info_retablir_lien' => 'Obnoviť tento odkaz',
 	'info_site_attente' => 'Stránka, ktorá čaká na schválenie',
-	'info_site_propose' => 'Site submitted on:',
-	'info_site_reference' => 'Referenced sites online',
+	'info_site_propose' => 'Stránka odoslaná:',
+	'info_site_reference' => 'Odkazované stránky online',
 	'info_site_refuse' => 'Zamietnutá stránka',
 	'info_site_syndique' => 'This site is syndicated...',
 	'info_site_valider' => 'Stránky, ktoré čakajú na schválenie',
-	'info_sites_referencer' => 'Reference a site',
-	'info_sites_refuses' => 'Rejected sites',
-	'info_statut_site_1' => 'This site is:',
-	'info_statut_site_2' => 'Published',
-	'info_statut_site_3' => 'Submitted',
+	'info_sites_referencer' => 'Odkázať na stránku',
+	'info_sites_refuses' => 'Zamietnuté stránky',
+	'info_statut_site_1' => 'Táto stránka je:',
+	'info_statut_site_2' => 'Publikovaná',
+	'info_statut_site_3' => 'Odoslaná',
 	'info_statut_site_4' => 'Do koša',
 	'info_syndication' => 'syndikácia:',
-	'info_syndication_articles' => 'článok (-ky)',
-	'item_bloquer_liens_syndiques' => 'Block syndicated links for validation',
-	'item_gerer_annuaire_site_web' => 'Manage Web sites directory',
+	'info_syndication_articles' => 'článok (-nky)',
+	'item_bloquer_liens_syndiques' => 'Zablokovať syndikované odkazy na overenie',
+	'item_gerer_annuaire_site_web' => 'Riadiť priečinok s internetovými stránkami',
 	'item_non_bloquer_liens_syndiques' => 'Neblokovať odkazy zo syndikácie',
-	'item_non_gerer_annuaire_site_web' => 'Disable Web sites directory',
+	'item_non_gerer_annuaire_site_web' => 'Deaktivovať priečinok s internetovými stránkami',
 	'item_non_utiliser_syndication' => 'Nepoužívať automatickú syndikáciu',
 	'item_utiliser_syndication' => 'Používať automatickú syndikáciu',
 
 	// L
-	'label_exporter_avec_mots_cles_1' => 'Exporter les mots-clés sous forme de tags', # NEW
-	'label_exporter_id_parent' => 'Exporter les sites de la rubrique', # NEW
-	'label_exporter_publie_seulement_1' => 'Exporter uniquement les sites publiés', # NEW
-	'label_fichier_import' => 'Fichier HTML', # NEW
-	'label_importer_les_tags_1' => 'Importer les tags sous forme de mot-clé', # NEW
-	'label_importer_statut_publie_1' => 'Publier automatiquement les sites', # NEW
-	'lien_mise_a_jour_syndication' => 'Update now',
-	'lien_nouvelle_recuperation' => 'Try to perform a new retrieval of data',
+	'label_exporter_avec_mots_cles_1' => 'Exportovať kľúčové slová vo forme tagov',
+	'label_exporter_id_parent' => 'Exportovať stránky z rubriky',
+	'label_exporter_publie_seulement_1' => 'Exportovať iba publikované stránky',
+	'label_fichier_import' => 'Súbor HTML',
+	'label_importer_les_tags_1' => 'Nahrať tagy vo forme kľúčových slov',
+	'label_importer_statut_publie_1' => 'Automaticky publikovať stránky',
+	'lien_mise_a_jour_syndication' => 'Aktualizovať teraz',
+	'lien_nouvelle_recuperation' => 'Pokúsiť sa znova získať dáta',
 	'lien_purger_syndication' => 'Odstrániť všetky syndikované články',
 
 	// N
@@ -108,21 +108,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'statut_off' => 'Odstránené',
 	'statut_prop' => 'Čaká',
 	'statut_publie' => 'Publikovaný',
-	'syndic_choix_moderation' => 'What should be done with the next links from this site?',
-	'syndic_choix_oublier' => 'What should be done with links which are no longer present in the syndication file?',
-	'syndic_choix_resume' => 'Some sites offer the full text of their articles. When the full text is available, do you wish to syndicate:',
-	'syndic_lien_obsolete' => 'obsolete link',
+	'syndic_choix_moderation' => 'Čo urobiť s odkazmi z tejto stránky?',
+	'syndic_choix_oublier' => 'Čo urobiť s odkazmi, ktoré už viac neexistujú v syndikačnom súbore?',
+	'syndic_choix_resume' => 'Niektoré stránky ponúkajú úplné texty svojich článkov. Ak je k dispozícii úplný text, chcete syndikovať:',
+	'syndic_lien_obsolete' => 'zastaraný odkaz',
 	'syndic_option_miroir' => 'zablokovať ich automaticky',
-	'syndic_option_oubli' => 'delete them (after @mois@ months)',
+	'syndic_option_oubli' => 'vymazať ich (po @mois@ mesiacoch)',
 	'syndic_option_resume_non' => 'celý obsah článkov (formát HTML)',
-	'syndic_option_resume_oui' => 'just a summary (text format)',
-	'syndic_options' => 'Syndication options:',
+	'syndic_option_resume_oui' => 'iba zhrnutie (ako text)',
+	'syndic_options' => 'Možnosti syndikácie:',
 
 	// T
-	'texte_expliquer_export_bookmarks' => 'Vous pouvez exporter une liste de sites au format Marque-page HTML,
-	pour vous permettre ensuite de l\'importer dans votre navigateur ou dans un service en ligne', # NEW
-	'texte_expliquer_import_bookmarks' => 'Vous pouvez importer une liste de sites au format Marque-page HTML,
-	en provenance de votre navigateur ou d\'un service en ligne de gestion des Marques-pages.', # NEW
+	'texte_expliquer_export_bookmarks' => 'Zoznam svojich záložiek môžete exportovať, aby ste ich nahrali do svojho prehliadača alebo internetovej služby na správu záložiek',
+	'texte_expliquer_import_bookmarks' => 'Zoznam záložiek vo formáte HTML môžete nahrať zo svojho prehliadača alebo internetovej služby na správu záložiek.',
 	'texte_liens_sites_syndiques' => 'Links emanating from syndicated sites could
 			be blocked beforehand; the following
 			setting show the default setting of
@@ -132,26 +130,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 			choose, for each site, to block the links coming
 			from any particular site.',
 	'texte_messages_publics' => 'Verejné príspevky k článku:',
-	'texte_non_fonction_referencement' => 'You can choose not to use this automated feature, and enter the elements concerning that site manually...',
+	'texte_non_fonction_referencement' => 'Môžete sa rozhodnúť nepoužívať túto automatizovanú funkciu a zadávať prvky z danej stránky manuálne.',
 	'texte_referencement_automatique' => '<b>Automatické odkazovanie na stránky</b><br />Na stránku môžete odkázať rýchlo uvedením jej adresy alebo adresy jej syndikačného súboru. SPIP automaticky získa údaje o stránke (názov, popis...).',
-	'texte_referencement_automatique_verifier' => 'Please, verify the information provided by <tt>@url@</tt> before saving.',
+	'texte_referencement_automatique_verifier' => 'Pred uložením, prosím, skontrolujte údaje z <tt>@url@</tt>.',
 	'texte_syndication' => 'Zoznam nových funkcií sa dá získať automaticky, keď to stránka povoľuje
 Na to musíte aktivovať syndikáciu. 
   <blockquote><i>Niektorí poskytovatelia hostingu deaktivujú túto funkciu; 
  v tom prípade nemôžete využívať syndikáciu obsahu
    zo svojej stránky.</i></blockquote>',
-	'titre_articles_syndiques' => 'Syndicated articles pulled out from this site',
-	'titre_dernier_article_syndique' => 'Latest syndicated articles',
-	'titre_exporter_bookmarks' => 'Exporter des Marques-pages', # NEW
-	'titre_importer_bookmarks' => 'Importer des Marques-pages', # NEW
-	'titre_importer_exporter_bookmarks' => 'Importer et Exporter des Marques-pages', # NEW
-	'titre_page_sites_tous' => 'Odkazované stránky',
-	'titre_referencement_sites' => 'Sites referencing and syndication',
+	'titre_articles_syndiques' => 'Syndikované články stiahnuté z tejto stránky',
+	'titre_dernier_article_syndique' => 'Najnovšie syndikované články',
+	'titre_exporter_bookmarks' => 'Exportovať záložky',
+	'titre_importer_bookmarks' => 'Nahrať záložky',
+	'titre_importer_exporter_bookmarks' => 'Nahrať a exportovať záložky',
+	'titre_page_sites_tous' => 'Stránky, na ktoré odkazujete',
+	'titre_referencement_sites' => 'Odkazovanie na stránky a ich syndikácia',
 	'titre_site_numero' => 'ČÍSLO STRÁNKY:',
-	'titre_sites_proposes' => 'Submitted sites',
+	'titre_sites_proposes' => 'Odoslané stránky',
 	'titre_sites_references_rubrique' => 'Odkazované stránky v tejto rubrike',
-	'titre_sites_syndiques' => 'Syndicated sites',
-	'titre_sites_tous' => 'Referenced sites',
+	'titre_sites_syndiques' => 'Syndikované stránky',
+	'titre_sites_tous' => 'Stránky, na ktoré odkazujete',
 	'titre_syndication' => 'Syndikácia stránok',
 	'tout_voir' => 'Zobraziť všetky syndikované články',
 
