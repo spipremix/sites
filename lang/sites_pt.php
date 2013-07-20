22b525af1646abe43b1ed1bb9e4953ed589356ef
salvatore <salvatore@rezo.net>
2013-07-20 03:37:25 +0000

    langues (sites)

--- lang/sites_pt.php
+++ lang/sites_pt.php
@@ -31,13 +31,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_radio_syndication' => 'Vinculação',
 
 	// C
-	'confirmer_purger_syndication' => 'Êtes-vous certain de vouloir supprimer tous les articles syndiqués de ce site ?', # NEW
+	'confirmer_purger_syndication' => 'Tem a certeza de que pretende eliminar todos os artigos vinculados deste sítio web ?',
 
 	// E
 	'entree_adresse_fichier_syndication' => 'Endereço do ficheiro  « backend » para a vinculação :',
 	'entree_adresse_site' => '<b>Endereço do sítio</b> [Obrigatório]',
 	'entree_description_site' => 'Descrição do sítio',
-	'erreur_fichier_format_inconnu' => 'Le format du fichier @fichier@ n\'est pas pris en charge.', # NEW
+	'erreur_fichier_format_inconnu' => 'O formato do ficheiro @fichier@ não é suportado.',
 	'erreur_fichier_incorrect' => 'Impossível ler o ficheiro.',
 
 	// F
@@ -46,18 +46,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// I
 	'icone_article_syndic' => 'Artigo vinculado',
 	'icone_articles_syndic' => 'Artigos vinculados',
-	'icone_controler_syndication' => 'Publication des articles syndiqués', # NEW
+	'icone_controler_syndication' => 'Publicação dos artigos vinculados',
 	'icone_modifier_site' => 'Modificar este sítio',
 	'icone_referencer_nouveau_site' => 'Referenciar um novo sítio',
 	'icone_site_reference' => 'Sítios Web referenciados',
 	'icone_supprimer_article' => 'Eliminar este artigo',
 	'icone_supprimer_articles' => 'Eliminar estes artigos',
 	'icone_valider_article' => 'Validar este artigo',
-	'icone_valider_articles' => 'Valider ces articles', # NEW
+	'icone_valider_articles' => 'Validar estes artigos',
 	'icone_voir_sites_references' => 'Ver os sítios referenciados',
 	'info_1_site_importe' => '1 sítio foi importado',
 	'info_a_valider' => '[a validar]',
-	'info_aucun_site_importe' => 'Aucun site n\'a pu être importé', # NEW
+	'info_aucun_site_importe' => 'Nenhum sítio web foi importado',
 	'info_bloquer' => 'bloquear',
 	'info_bloquer_lien' => 'bloquear esta ligação',
 	'info_derniere_syndication' => 'A última vinculação deste sítio foi efectuada a',
@@ -91,45 +91,43 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'item_utiliser_syndication' => 'Utilizar a vinculação automática',
 
 	// L
-	'label_exporter_avec_mots_cles_1' => 'Exporter les mots-clés sous forme de tags', # NEW
-	'label_exporter_id_parent' => 'Exporter les sites de la rubrique', # NEW
-	'label_exporter_publie_seulement_1' => 'Exporter uniquement les sites publiés', # NEW
-	'label_fichier_import' => 'Fichier HTML', # NEW
-	'label_importer_les_tags_1' => 'Importer les tags sous forme de mot-clé', # NEW
-	'label_importer_statut_publie_1' => 'Publier automatiquement les sites', # NEW
+	'label_exporter_avec_mots_cles_1' => 'Exportar as palavras-chave como tags',
+	'label_exporter_id_parent' => 'Exportar os sítios web da rubrica',
+	'label_exporter_publie_seulement_1' => 'Exportar unicamente os sítios web publicados',
+	'label_fichier_import' => 'Ficheiro HTML',
+	'label_importer_les_tags_1' => 'Importar tags como palavras-chave',
+	'label_importer_statut_publie_1' => 'Publicar automaticamente os sítios web',
 	'lien_mise_a_jour_syndication' => 'Actualizar agora',
 	'lien_nouvelle_recuperation' => 'Tentar uma nova recuperação dos dados',
-	'lien_purger_syndication' => 'Effacer tous les articles syndiqués', # NEW
+	'lien_purger_syndication' => 'Eliminar todos os artigos vinculados',
 
 	// N
-	'nombre_articles_syndic' => '@nb@ articles syndiqués', # NEW
+	'nombre_articles_syndic' => '@nb@ artigos vinculados',
 
 	// S
-	'statut_off' => 'Supprimé', # NEW
-	'statut_prop' => 'En attente', # NEW
-	'statut_publie' => 'Publié', # NEW
+	'statut_off' => 'Eliminado',
+	'statut_prop' => 'Em espera',
+	'statut_publie' => 'Publicado',
 	'syndic_choix_moderation' => 'Que fazer com as novas ligações provenientes deste sítio ?',
 	'syndic_choix_oublier' => 'Que fazer com as ligações que já não aparecem no ficheiro de vinculação?',
-	'syndic_choix_resume' => 'Certains sites diffusent le texte complet des articles. Lorsque celui-ci est disponible souhaitez-vous syndiquer :', # NEW
+	'syndic_choix_resume' => 'Certos sítios incluem o texto completo dos artigos. Quando este se encontra disponível, pretende vincular:',
 	'syndic_lien_obsolete' => 'ligação desactualizada',
 	'syndic_option_miroir' => 'bloquear automaticamente',
 	'syndic_option_oubli' => 'eliminar (após @mês@ mês)',
-	'syndic_option_resume_non' => 'le contenu complet des articles (au format HTML)', # NEW
-	'syndic_option_resume_oui' => 'un simple résumé (au format texte)', # NEW
+	'syndic_option_resume_non' => 'o conteúdo completo dos artigos (em formato HTML)',
+	'syndic_option_resume_oui' => 'apenas um resumo (em formato de texto)',
 	'syndic_options' => 'Opções de vinculação :',
 
 	// T
-	'texte_expliquer_export_bookmarks' => 'Vous pouvez exporter une liste de sites au format Marque-page HTML,
-	pour vous permettre ensuite de l\'importer dans votre navigateur ou dans un service en ligne', # NEW
-	'texte_expliquer_import_bookmarks' => 'Vous pouvez importer une liste de sites au format Marque-page HTML,
-	en provenance de votre navigateur ou d\'un service en ligne de gestion des Marques-pages.', # NEW
+	'texte_expliquer_export_bookmarks' => 'Pode exportar uma lista de sítios web com o formato de marcador HTML, para que possa importá-la para o seu navegador ou para um serviço online.',
+	'texte_expliquer_import_bookmarks' => 'Pode importar uma lista de sítios web a partir do formato Marcador HTML, do seu navegador ou de outro serviço online de gestão de marcadores.',
 	'texte_liens_sites_syndiques' => 'As ligações de sítios vinculados podem
  ser bloqueados antes da publicação. A seguinte definição a configuração por defeito dos
  sítios vinculados depois da sua criação. É possível, de qualquer modo, desbloquear cada ligação individualmente, ou escolher, sítio por sítio, bloquear as ligações futuras deste ou daquele sítio.',
 	'texte_messages_publics' => 'Mensagens públicas neste artigo :',
 	'texte_non_fonction_referencement' => 'Pode preferir não utilizar esta função automática, e indicar manualmente os elementos relativos a este sítio...',
 	'texte_referencement_automatique' => '<b>Referenciamento automatizado de um sítio</b><br />Pode referenciar rapidamente um sítio Web indicando a seguir o endereço URL desejado, ou o endereço do seu ficheiro de vinculação. SPIP vai recuperar automaticamente as informações relativas a esse sítio (título, descrição...).',
-	'texte_referencement_automatique_verifier' => 'Veuillez vérifier les informations fournies par <tt>@url@</tt> avant d\'enregistrer.', # NEW
+	'texte_referencement_automatique_verifier' => 'Por favor verifique a informação fornecida por <tt>@url@</tt> antes de guardar.',
 	'texte_syndication' => 'É possível recuperar automaticamente, quando um sítio Web o permitir, 
  a lista das suas novidades. Para tal, deve activar a vinculação. 
   <blockquote><i>Alguns serviços de hospedagem desactivam esta funcionalidade ; 
@@ -137,9 +135,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 a partir do seu sítio.</i></blockquote>',
 	'titre_articles_syndiques' => 'Artigos vinculados tirados deste sítio',
 	'titre_dernier_article_syndique' => 'Últimos artigos vinculados',
-	'titre_exporter_bookmarks' => 'Exporter des Marques-pages', # NEW
-	'titre_importer_bookmarks' => 'Importer des Marques-pages', # NEW
-	'titre_importer_exporter_bookmarks' => 'Importer et Exporter des Marques-pages', # NEW
+	'titre_exporter_bookmarks' => 'Exportar os Marcadores',
+	'titre_importer_bookmarks' => 'Importar os Marcadores',
+	'titre_importer_exporter_bookmarks' => 'Importar e exportar os Marcadores',
 	'titre_page_sites_tous' => 'Os sítios referenciados',
 	'titre_referencement_sites' => 'Referenciação de sítios e vinculação',
 	'titre_site_numero' => 'SÍTIO NÚMERO :',
@@ -148,10 +146,10 @@ a partir do seu sítio.</i></blockquote>',
 	'titre_sites_syndiques' => 'Os sítios vinculados',
 	'titre_sites_tous' => 'Os sítios referenciados',
 	'titre_syndication' => 'Vinculação de sítios',
-	'tout_voir' => 'Voir tous les articles syndiqués', # NEW
+	'tout_voir' => 'Ver todos os artigos vinculados',
 
 	// U
-	'un_article_syndic' => '1 article syndiqué' # NEW
+	'un_article_syndic' => '1 artigo vinculado'
 );
 
 ?>
