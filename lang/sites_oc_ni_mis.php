1cb78e0a6b5020e4668e4174f5e1b3283faee41f
sylvain <sylvain@oliver-fr.org>
2015-02-27 04:33:07 +0000

    [Salvatore] [source:_core_/plugins/sites/lang/ sites] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/sites@87658 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/sites_oc_ni_mis.php
@@ -0,0 +1,104 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/sites?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'articles_dispo' => 'En aspera',
+	'articles_meme_auteur' => 'Toui lu article d’aquel autour',
+	'articles_off' => 'Bloucat',
+	'articles_publie' => 'Publicat',
+	'articles_refuse' => 'Suprimat',
+	'articles_tous' => 'Toui',
+	'aucun_article_syndic' => 'Mìnga article sindicat',
+	'avis_site_introuvable' => 'Sit introuvable',
+
+	// B
+	'bouton_exporter' => 'Espourtà',
+	'bouton_importer' => 'Empourtà',
+	'bouton_radio_non_syndication' => 'Mìnga de sindicacioun',
+	'bouton_radio_syndication' => 'Sindicacioun :',
+
+	// E
+	'erreur_fichier_incorrect' => 'Empoussible de legì lou fichié.',
+
+	// F
+	'form_prop_nom_site' => 'Noum dóu sit',
+
+	// I
+	'icone_article_syndic' => 'Article sindicat',
+	'icone_articles_syndic' => 'Article sindicat',
+	'icone_controler_syndication' => 'Publicacioun dei article sindicat',
+	'icone_modifier_site' => 'Moudificà aqueu sit',
+	'icone_referencer_nouveau_site' => 'Referençà un nouvèu sit',
+	'icone_site_reference' => 'Sit referençat',
+	'icone_supprimer_article' => 'Suprimà aquel article',
+	'icone_supprimer_articles' => 'Suprimà aquelu article',
+	'icone_valider_article' => 'Validà aquel article',
+	'icone_valider_articles' => 'Validà aquelu article',
+	'icone_voir_sites_references' => 'Veire lu sit referençat',
+	'info_1_article_syndique' => '1 article sindicat',
+	'info_1_site' => '1 sit',
+	'info_1_site_importe' => '1 sit es estat empourtat',
+	'info_a_valider' => '[da validà]',
+	'info_aucun_article_syndique' => 'Mìnga article sindicat',
+	'info_aucun_site' => 'Mìnga sit',
+	'info_aucun_site_importe' => 'Mìnga sit si pòu estre empourtat',
+	'info_bloquer' => 'bloucà',
+	'info_bloquer_lien' => 'bloucà aqueu sit',
+	'info_liens_syndiques_1' => 'estac sindicat',
+	'info_liens_syndiques_2' => 'soun en aspera de validacioun.',
+	'info_nb_articles_syndiques' => '@nb@ article sindicat',
+	'info_nb_sites' => '@nb@ sit',
+	'info_nb_sites_importes' => '@nb@ sit soun estat empourtat',
+	'info_site_attente' => 'Sit Web en aspera de validacioun',
+	'info_site_reference' => 'Sit referençat en ligna',
+	'info_site_refuse' => 'Sit Web refudat',
+	'info_site_syndique' => 'Aqueu sit es sindicat…',
+	'info_site_valider' => 'Sit da validà',
+	'info_sites_refuses' => 'Lu sit refudat',
+	'info_statut_site_1' => 'Aqueu sit es :',
+	'info_statut_site_2' => 'Publicat',
+	'info_syndication' => 'sindicacioun :',
+	'info_syndication_articles' => 'article',
+	'item_bloquer_liens_syndiques' => 'Bloucà lu estac sindicat per validacioun',
+	'item_gerer_annuaire_site_web' => 'Gerà un anuàri de sit Web',
+	'item_non_bloquer_liens_syndiques' => 'Noun bloucà lu estac que venon de la sindicacioun',
+	'item_non_gerer_annuaire_site_web' => 'Desativà l’anuàri de sit Web',
+	'item_non_utiliser_syndication' => 'Noun utilisà la sindicacioun automatica',
+	'item_utiliser_syndication' => 'Utilisà la sindicacioun automatica',
+
+	// L
+	'label_exporter_publie_seulement_1' => 'Espourtà unicamen lu sit publicat',
+	'label_fichier_import' => 'Fichié HTML',
+	'label_importer_statut_publie_1' => 'Publicà automaticamen lu sit',
+	'lien_mise_a_jour_syndication' => 'Metre a jou ahura',
+
+	// N
+	'nombre_articles_syndic' => '@nb@ article sindicat',
+
+	// S
+	'statut_off' => 'Suprimat',
+	'statut_prop' => 'En aspera',
+	'statut_publie' => 'Publicat',
+
+	// T
+	'texte_messages_publics' => 'Message public dau article :',
+	'texte_non_fonction_referencement' => 'Poudès chausì de noun utilisà aquela founcioun automatica, e endicà vous meme lu elemen que tocon aqueu sit...',
+	'texte_syndication' => 'Si pòu recuperà automaticamen, quoura un sit web lou permet,
+la lista dei sieu nouvità. Per faire acò, vous cau ativà la sindicacioun. 
+	<blockquote><i>D’unu aubergadour noun ativon aquela founciounalità ; en aqueu cas, noun pourès utilisà la sindicacioun de countengut despì lou vouòstre sit.</i></blockquote>',
+	'titre_site_numero' => 'SIT NÙMERO :',
+	'titre_sites_syndiques' => 'Lu sit sindicat',
+	'titre_syndication' => 'Sindicacioun de sit',
+	'tout_voir' => 'Veire toui lu article sindicat',
+
+	// U
+	'un_article_syndic' => '1 article sindicat'
+);
+
+?>
