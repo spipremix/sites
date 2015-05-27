1b182fde292b1146117f0ad968b32e268160362c
olly.olga <olly.olga@gmail.com>
2015-05-27 03:33:52 +0000

    [Salvatore] [source:_core_/plugins/sites/lang/ sites] Export depuis http://trad.spip.net de la langue uk

--- lang/sites_uk.php
+++ lang/sites_uk.php
@@ -67,6 +67,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_derniere_syndication' => 'Дата останнього імпорту материалів ',
 	'info_liens_syndiques_1' => 'імпортованих статтей',
 	'info_liens_syndiques_2' => 'чекають на затвердження.',
+	'info_nb_articles_syndiques' => '@nb@ статей, що реферуються',
 	'info_nb_sites' => '@nb@ сайти',
 	'info_nb_sites_importes' => 'Було імпортовано @nb@ сайтів',
 	'info_nom_site_2' => '<b>Назва сайту</b> [обов’язково]',
