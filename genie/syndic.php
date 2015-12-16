0dc72deccbe23842ed4b1ef6d19e60c87782e459
Cerdic <cedric@yterium.com>
2015-12-16 09:57:04 +0000

    Report de r93712 : passer l'action 'syndiquer' a objet_modifier_champs

--- genie/syndic.php
+++ genie/syndic.php
@@ -347,7 +347,7 @@ function inserer_article_syndique($data, $now_id_syndic, $statut, $url_site, $ur
 	}
 
 	include_spip('inc/modifier');
-	objet_modifier_champs('syndic_article',$id_syndic_article,array('data'=>$vals),$vals);
+	objet_modifier_champs('syndic_article',$id_syndic_article,array('data'=>$vals,'action'=>'syndiquer'),$vals);
 
 	// Point d'entree post_syndication
 	pipeline('post_syndication',
