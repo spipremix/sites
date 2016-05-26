7fee3006880791b489ff148d2beab4c9727bfdc9
Cerdic <cedric@yterium.com>
2016-05-26 16:16:19 +0000

    Report de r98071 : Report de r98068 : Certains flux RSS publient l'integralite de leur contenus (par exemple http://www.lelotenaction.org/pages/do/rss.xml) ce qui produit inevitablement un timeout lors de l'analyse et de la syndication. On optimise les fonctions cdata_echappe et cdata_echappe_retour, et on limite la syndication aux 1000 premiers items, surchargeable par la constante _SYNDICATION_MAX_ITEMS
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/sites@98073 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- inc/syndic.php
+++ inc/syndic.php
@@ -100,8 +100,13 @@ function analyser_backend($rss, $url_syndic = '') {
 		return _T('sites:avis_echec_syndication_01');
 	}
 
+	if (!defined('_SYNDICATION_MAX_ITEMS')) define('_SYNDICATION_MAX_ITEMS',1000);
+	$nb_items = 0;
 	foreach ($items as $item) {
 		$data = array();
+		if ($nb_items++>_SYNDICATION_MAX_ITEMS){
+			break;
+		}
 
 		// URL (semi-obligatoire, sert de cle)
 
@@ -553,9 +558,14 @@ function cdata_echappe(&$rss, &$echappe_cdata) {
 	if (preg_match_all(',<!\[CDATA\[(.*)]]>,Uims', $rss,
 		$regs, PREG_SET_ORDER)) {
 		foreach ($regs as $n => $reg) {
-			if (preg_match(',[<>],', $reg[1])) {
-				$echappe_cdata[$n] = $reg[1];
-				$rss = str_replace($reg[0], "@@@SPIP_CDATA$n@@@", $rss);
+			if (strpos($reg[1],'<')!==false
+			  or strpos($reg[1],'>')!==false) {
+				// verifier que la chaine est encore dans le flux, car on peut avoir X fois la meme
+				// inutile de (sur)peupler le tableau avec des substitutions identiques
+				if (strpos($rss,$reg[0])!==false){
+					$echappe_cdata["@@@SPIP_CDATA$n@@@"] = $reg[1];
+					$rss = str_replace($reg[0], "@@@SPIP_CDATA$n@@@", $rss);
+				}
 			} else {
 				$rss = str_replace($reg[0], $reg[1], $rss);
 			}
@@ -567,13 +577,11 @@ function cdata_echappe(&$rss, &$echappe_cdata) {
 // http://code.spip.net/@cdata_echappe_retour
 function cdata_echappe_retour(&$x, &$echappe_cdata) {
 	if (is_string($x)) {
-		if (strpos($x, '@@@SPIP_CDATA') !== false
-			or strpos($x, '&lt;') !== false
-		) {
+		if (strpos($x, '&lt;') !== false){
 			$x = filtrer_entites($x);
-			foreach ($echappe_cdata as $n => $e) {
-				$x = str_replace("@@@SPIP_CDATA$n@@@", $e, $x);
-			}
+		}
+		if (strpos($x, '@@@SPIP_CDATA') !== false){
+			$x = str_replace( array_keys($echappe_cdata), array_values($echappe_cdata), $x);
 		}
 	} else {
 		if (is_array($x)) {
