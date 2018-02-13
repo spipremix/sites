dac52759e0ad701df13ac91ea06fbe24557f846c
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-13 04:37:04 +0000

    [Salvatore] [source:_core_/plugins/sites/lang/ sites] Export depuis http://trad.spip.net de la langue br

--- lang/sites_br.php
+++ lang/sites_br.php
@@ -14,9 +14,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'articles_meme_auteur' => 'Bep pennadou gant ar saver-mañ',
 	'articles_off' => 'Stanket',
 	'articles_publie' => 'Embannnet',
-	'articles_refuse' => 'Lazhet', # RELIRE
+	'articles_refuse' => 'Dilemet',
 	'articles_tous' => 'Pep tra',
-	'aucun_article_syndic' => 'N’eus pennad-skrid sindikaturet ebet',
+	'aucun_article_syndic' => 'N’eus pennad-skrid sindikadet ebet',
 	'avis_echec_syndication_01' => 'C’hwitet eo bet ar sindikadur : dilennus eo ar backend kinniget, pe ne ginnig pennad ebet.',
 	'avis_echec_syndication_02' => 'C’hwitet eo bet ar sindikadur : n’eus ket tu da dizhout backend al lec’hienn-mañ.',
 	'avis_site_introuvable' => 'N’haller ket kavout al lec’hienn-se',
@@ -25,47 +25,53 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'avis_sites_syndiques_probleme' => 'Kudennoù zo savet gant al lec’hiennoù sindikadet-se',
 
 	// B
-	'bouton_exporter' => 'Ezkas', # RELIRE
+	'bouton_exporter' => 'Ezporzhiañ',
 	'bouton_importer' => 'Dikas',
 	'bouton_radio_modere_posteriori' => 'adwelet goude an embann',
 	'bouton_radio_modere_priori' => 'adwelet a-raok an embann',
 	'bouton_radio_non_syndication' => 'Sindikadur ebet',
 	'bouton_radio_syndication' => 'Sindikadur :',
 
+	// C
+	'confirmer_purger_syndication' => 'Hoc’h eus c’hoant da zilemel an holl pennadoù-skrid sindikadet deus al lec’hienn-mañ ?',
+
 	// E
 	'entree_adresse_fichier_syndication' => 'Chomlec’h ar restr evit ar sindikadur :',
 	'entree_adresse_site' => '<b>Chomlec’h al lec’hienn</b> [Rekis]',
 	'entree_description_site' => 'Deskrivadenn al lec’hienn',
+	'erreur_fichier_format_inconnu' => 'N’eo ket kemeret furmad ar restr @fichier@ e karg.',
 	'erreur_fichier_incorrect' => 'Diposubl da lenn ar fichennaoueg',
 
 	// F
 	'form_prop_nom_site' => 'Anv al lec’hienn',
 
 	// I
-	'icone_article_syndic' => 'Pennad-skrid sindikaturet',
-	'icone_articles_syndic' => 'Pennadoù-skrid sindikaturet',
-	'icone_controler_syndication' => 'Embannadenn ar pennadoù-skrid sindikaturet',
+	'icone_article_syndic' => 'Pennad-skrid sindikadet',
+	'icone_articles_syndic' => 'Pennadoù-skrid sindikadet',
+	'icone_controler_syndication' => 'Embannadenn ar pennadoù-skrid sindikadet',
 	'icone_modifier_site' => 'Kemmañ al lec’hienn-mañ',
 	'icone_referencer_nouveau_site' => 'Menegeriñ ul lec’hienn nevez',
 	'icone_site_reference' => 'Lec’hioù daveennet',
-	'icone_supprimer_article' => 'Larzh ar pennad-mañ',
+	'icone_supprimer_article' => 'Dilemel ar pennad-mañ',
 	'icone_supprimer_articles' => 'Dilemel ar pennadoù-skrid-mañ',
-	'icone_valider_article' => 'Gwirian ar pennad-mañ',
+	'icone_valider_article' => 'Gwiriañ ar pennad-mañ',
 	'icone_valider_articles' => 'Gwirian ar pennad-mañ',
 	'icone_voir_sites_references' => 'Gwelet al lec’hiennoù menegeret',
 	'info_1_article_syndique' => '1 pennad-skrid sindikadet',
 	'info_1_site' => '1 lec’hienn',
 	'info_1_site_importe' => 'Unan lech’ienn a zo bet ezkas',
 	'info_a_valider' => '[da wiriañ]',
-	'info_aucun_article_syndique' => 'Pennad-skrid amaezhiet ebet',
+	'info_aucun_article_syndique' => 'Pennad-skrid sindikadet ebet',
 	'info_aucun_site' => 'lec’hienn ebet',
+	'info_aucun_site_importe' => 'Bez eus enporzhiet lec’hienn ebet',
 	'info_bloquer' => 'stankañ',
 	'info_bloquer_lien' => 'Stankañ al liamm-mañ',
 	'info_derniere_syndication' => 'Sindikadur diwezhañ al lec’hienn-mañ a zo bet graet d’an',
 	'info_liens_syndiques_1' => 'Liammoù sindikadet',
 	'info_liens_syndiques_2' => 'o c’hortoz bezañ asantet.',
-	'info_nb_articles_syndiques' => '@nb@ pennadoù-skrid amaezhiet',
+	'info_nb_articles_syndiques' => '@nb@ pennadoù-skrid sindikadet ',
 	'info_nb_sites' => '@nb@ lec’hiennoù',
+	'info_nb_sites_importes' => '@nb@ lec’hiennoù zo bet enporzhiet',
 	'info_nom_site_2' => '<b>Anv al lec’hienn</b> [Rekis]',
 	'info_panne_site_syndique' => 'Sac’het eo al lec’hienn sindikadet',
 	'info_probleme_grave' => 'kudenn a',
@@ -87,18 +93,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_syndication_articles' => 'pennad(où)',
 	'item_bloquer_liens_syndiques' => 'Stankañ al liammoù sindikadet evit o c’hadarnaat',
 	'item_gerer_annuaire_site_web' => 'Merañ ur roll lec’hiennoù Web',
-	'item_non_bloquer_liens_syndiques' => 'Arabat stankañ al liammoù deuet diwar ar sindikadañ',
+	'item_non_bloquer_liens_syndiques' => 'Arabat stankañ al liammoù deuet diwar ar sindikadur',
 	'item_non_gerer_annuaire_site_web' => 'Diweredekaat roll al lec’hiennoù Web',
-	'item_non_utiliser_syndication' => 'Na implijout ar sindikadañ emgefre',
+	'item_non_utiliser_syndication' => 'Na implijout ar sindikadur emgefre',
 	'item_utiliser_syndication' => 'Implijout ar sindikadur emgefre',
 
 	// L
+	'label_exporter_id_parent' => 'Ezhporzhiañ lec’hiennoù ar rubrikenn',
 	'label_exporter_publie_seulement_1' => ' Digas nemetken ar lec’hiennoù embannet',
+	'label_fichier_import' => 'Restr HTML',
+	'label_importer_statut_publie_1' => 'Enbann emgefreek al lec’hiennoù',
 	'lien_mise_a_jour_syndication' => 'Hizivaat bremañ',
 	'lien_nouvelle_recuperation' => 'Klask adtapout an titouroù en-dro',
+	'lien_purger_syndication' => 'Diverkañ an holl pennadoù-skrid sindikadet',
+
+	// N
+	'nombre_articles_syndic' => '@nb@ pennadoù-skrid sindikadet',
 
 	// S
-	'statut_off' => 'Larzhet',
+	'statut_off' => 'Dilemet',
 	'statut_prop' => 'En gortoz',
 	'statut_publie' => 'Embannet',
 	'syndic_choix_moderation' => 'Petra ober gant al liammoù a zeu o tont eus al lec’hienn-mañ ?',
@@ -109,7 +122,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'syndic_option_oubli' => 'diverkañ anezho (goude @mois@ miz)',
 	'syndic_option_resume_non' => 'endalc’had klok ar pennadoù (er furmad HTML)',
 	'syndic_option_resume_oui' => 'un diverrañ hepken (e furmad testenn)',
-	'syndic_options' => 'Dibarzhioù sindikadañ :',
+	'syndic_options' => 'Dibarzhioù sindikadur :',
 
 	// T
 	'texte_liens_sites_syndiques' => 'Gallout a ra al liammoù deuet diwar lec’hiennoù sindikadet bezañ stanket a-raok embann ;
@@ -125,12 +138,19 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
   neuze n’hallit ket implijout ar sindikadur adalek ho lec’hienn.</i></blockquote>', # MODIF
 	'titre_articles_syndiques' => 'Pennadoù sindikadet tennet eus al lec’hienn-mañ',
 	'titre_dernier_article_syndique' => 'Pennadoù sindikadet da ziwezhañ',
+	'titre_exporter_bookmarks' => 'Ezporzhiañ Merkoù-pajenn',
+	'titre_importer_bookmarks' => 'Enporzhiañ Merkoù-pajenn',
+	'titre_importer_exporter_bookmarks' => 'Enporzhiañ hag Ezhporzhiañ Merkoù-pajenn',
 	'titre_page_sites_tous' => 'Al lec’hiennoù menegeret',
 	'titre_referencement_sites' => 'Menegeriñ lec’hiennoù ha sindikadañ',
 	'titre_site_numero' => 'LEC’HIENN NIVERENN :',
 	'titre_sites_proposes' => 'Al lec’hiennoù kinniget',
 	'titre_sites_references_rubrique' => 'Al lec’hiennoù menegeret er rummad-mañ',
-	'titre_sites_syndiques' => 'Al lec’hiennoù sindikadet dre RSS',
+	'titre_sites_syndiques' => 'Al lec’hiennoù sindikadet ',
 	'titre_sites_tous' => 'Al lec’hiennoù menegeret',
-	'titre_syndication' => 'Sindikadañ lec’hiennoù'
+	'titre_syndication' => 'Sindikadur al lec’hiennoù',
+	'tout_voir' => 'Gwelout an holl pennadoù-skrid sindikadet',
+
+	// U
+	'un_article_syndic' => '1 pennad-skrid sindikadet'
 );
