0b73e3d193e0cb5a03e9162869c701dbb389653f
gpl <gpl@long-player.net>
2018-03-15 04:42:12 +0000

    [Salvatore] [source:_core_/plugins/sites/lang/ sites] Export depuis http://trad.spip.net de la langue eo

--- lang/sites_eo.php
+++ lang/sites_eo.php
@@ -10,36 +10,60 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'articles_dispo' => 'Atendante',
+	'articles_meme_auteur' => 'Ĉiuj artikoloj el tiu aŭtoro',
+	'articles_off' => 'Blokitaj',
+	'articles_publie' => 'Publikigitaj',
+	'articles_refuse' => 'Forigitaj',
+	'articles_tous' => 'Ĉiuj',
+	'aucun_article_syndic' => 'Neniu abonligita artikolo',
 	'avis_echec_syndication_01' => 'Abonligo malsukcesis : la indikata abondosiero (backend-dosiero) ne estas deĉifrebla aŭ proponas neniun artikolon.',
 	'avis_echec_syndication_02' => 'Abonligo malsukcesis : ne eblas aliri abondosieron (backend-dosiero) de tiu retejo.',
 	'avis_site_introuvable' => 'Netrovebla retejo',
-	'avis_site_syndique_probleme' => 'Atentu, okazis problemo pri abonrilato de tiu ĉi retejo ; la sistemo estas portempe ĉesigita. Kontrolu la retadreson de la abondosiero de tiu ĉi retejo (@url_syndic@), kaj provu novan reakiron de la informoj.',
+	'avis_site_syndique_probleme' => 'Atentu, okazis problemo pri abonrilato de tiu ĉi retejo ; la sistemo estas portempe ĉesigita. Kontrolu la retadreson de la abondosiero de tiu ĉi retejo (<b>@url_syndic@</b>), kaj provu novan reakiron de la informoj.',
 	'avis_sites_probleme_syndication' => 'Okazis problemo pri abonrilato inter tiuj retejoj',
 	'avis_sites_syndiques_probleme' => 'Tiuj abonligitaj retejoj okazigis problemon',
 
 	// B
-	'bouton_radio_modere_posteriori' => 'aposteriore moderigata', # MODIF
-	'bouton_radio_modere_priori' => 'apriore moderigata', # MODIF
+	'bouton_exporter' => 'Eksporti',
+	'bouton_importer' => 'Importi',
+	'bouton_radio_modere_posteriori' => 'aposteriore moderigata',
+	'bouton_radio_modere_priori' => 'apriore moderigata',
 	'bouton_radio_non_syndication' => 'Neniu abonrilato',
 	'bouton_radio_syndication' => 'Abonrilato :',
+
+	// C
+	'confirmer_purger_syndication' => 'Ĉu vi certas, ke vi volas forigi ĉiujn abonligitajn artikolojn el tiu retejo ?',
 
 	// E
 	'entree_adresse_fichier_syndication' => 'Retadreso de la abondosiero :',
 	'entree_adresse_site' => '<b>Adreso de la retejo</b> [Deviga]',
 	'entree_description_site' => 'Priskribo de la retejo',
+	'erreur_fichier_format_inconnu' => 'La dosierformo el @fichier@ ne estas subtenata.',
+	'erreur_fichier_incorrect' => 'Ne eblas legi la dosieron.',
 
 	// F
 	'form_prop_nom_site' => 'Nomo de la retejo',
 
 	// I
+	'icone_article_syndic' => 'Abonligita artikolo',
+	'icone_articles_syndic' => 'Abonligitaj artikoloj',
+	'icone_controler_syndication' => 'Publikigo de la abonligitaj artikoloj',
 	'icone_modifier_site' => 'Modifi tiun ĉi retejon',
 	'icone_referencer_nouveau_site' => 'Referencigi novan retejon',
+	'icone_site_reference' => 'Referencigitaj retejoj',
+	'icone_supprimer_article' => 'Forigi tiun artikolon',
+	'icone_supprimer_articles' => 'Forigi tiujn artikolojn',
+	'icone_valider_article' => 'Validigi tiun artikolon',
+	'icone_valider_articles' => 'Validigi tiujn artikolojn',
 	'icone_voir_sites_references' => 'Vidi la referencigitajn retejojn',
 	'info_1_article_syndique' => '1 abonligita artikolo',
 	'info_1_site' => '1 retejo',
+	'info_1_site_importe' => '1 retejo estis importita',
 	'info_a_valider' => '[validigenda]',
 	'info_aucun_article_syndique' => 'Neniu abonligita artikolo',
 	'info_aucun_site' => 'Neniu retejo',
+	'info_aucun_site_importe' => 'Neniu retejo povis esti importita',
 	'info_bloquer' => 'bloki',
 	'info_bloquer_lien' => 'bloki tiun ligilon',
 	'info_derniere_syndication' => 'La lasta abonĝisdatigo de tiu retejo estis farita la ',
@@ -47,6 +71,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_liens_syndiques_2' => 'estas validigotaj.',
 	'info_nb_articles_syndiques' => '@nb@ abonligitaj artikoloj',
 	'info_nb_sites' => '@nb@ retejoj',
+	'info_nb_sites_importes' => '@nb@ retejoj estis importitaj',
 	'info_nom_site_2' => '<b>Nomo de la retejo</b> [Deviga]',
 	'info_panne_site_syndique' => 'Abonligita retejo paneas',
 	'info_probleme_grave' => 'problemo de',
@@ -56,14 +81,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_site_propose' => 'Retejo proponita la :',
 	'info_site_reference' => 'Retejo rete referencigita',
 	'info_site_refuse' => 'Rifuzita retejo',
-	'info_site_syndique' => 'Tiu ĉi retejo estas abonligita...', # MODIF
+	'info_site_syndique' => 'Tiu ĉi retejo estas abonligita...',
 	'info_site_valider' => 'Validigotaj retejoj',
 	'info_sites_referencer' => 'Referencigi retejon',
 	'info_sites_refuses' => 'La rifuzitaj retejoj',
 	'info_statut_site_1' => 'Tiu retejo estas :',
 	'info_statut_site_2' => 'Publikigita',
 	'info_statut_site_3' => 'Proponita',
-	'info_statut_site_4' => 'En rubujo', # MODIF
+	'info_statut_site_4' => 'En rubujo',
 	'info_syndication' => 'abonrilato :',
 	'info_syndication_articles' => 'artikolo(j)',
 	'item_bloquer_liens_syndiques' => 'Bloki la abonligilojn por validigo',
@@ -74,10 +99,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'item_utiliser_syndication' => 'Uzi la aŭtomatan abonrilaton',
 
 	// L
+	'label_exporter_avec_mots_cles_1' => 'Eksporti la ŝlosilojn kiel etikedojn',
+	'label_exporter_id_parent' => 'Eksporti la retejojn de la rubriko',
+	'label_exporter_publie_seulement_1' => 'Eksporti nur la publikigitajn retejojn',
+	'label_fichier_import' => 'HTML-dosiero',
+	'label_importer_les_tags_1' => 'Importi la etikedojn kiel ŝlosilojn',
+	'label_importer_statut_publie_1' => 'Publikigi aŭtomate la retejojn',
 	'lien_mise_a_jour_syndication' => 'Ĝisdatigi nun',
 	'lien_nouvelle_recuperation' => 'Provi novan reakiron de la datenoj',
+	'lien_purger_syndication' => 'Forigi ĉiujn abonligitajn artikolojn',
+
+	// N
+	'nombre_articles_syndic' => '@nb@ abonligitaj artikoloj',
 
 	// S
+	'statut_off' => 'Forigita',
+	'statut_prop' => 'Atendante',
+	'statut_publie' => 'Publikigita',
 	'syndic_choix_moderation' => 'Kion fari el la ligiloj venontaj el tiu retejo ?',
 	'syndic_choix_oublier' => 'Kion fari el la ligiloj, kiuj ne plu aperas en la abondosiero ?',
 	'syndic_choix_resume' => 'Iuj retejoj disponigas la tutan tekston de la artikoloj. Kiam tiu ĉi disponeblas, ĉu vi deziras abonligi :',
@@ -89,23 +127,29 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'syndic_options' => 'Abon-agordoj :',
 
 	// T
+	'texte_expliquer_export_bookmarks' => 'Vi povas eksporti liston de retejoj sub la dosierformo „HTML-paĝosigno“, kiu ebligos vin, poste, importi ĝin en via retumilon aŭ en reta servo.',
+	'texte_expliquer_import_bookmarks' => 'Vi povas importi liston de retejoj sub la dosierformo  „HTML-paĝosigno“, kiu elvenas el via retumilo aŭ el reta servo pri mastrumado de paĝosignoj.',
 	'texte_liens_sites_syndiques' => 'Abonligoj povas
    esti blokataj apriore ; la ĉi-suba agordo
    indikas la defaŭltan agordon de la
    abonligitaj retejoj je ties kreo. Poste
    eblas ĉiukaze malbloki ĉiun
    ligilon unuope, aŭ elekti, retejon post retejo, bloki estigotajn
-ligilojn al jena aŭ alia retejo.', # MODIF
+ligilojn al jena aŭ alia retejo.',
 	'texte_messages_publics' => 'Publikaj mesaĝoj de la artikolo :',
-	'texte_non_fonction_referencement' => 'Vi povas preferi ne uzi tiun aŭtomatan funkcion kaj indiki mem la elementojn koncernantajn tiun ĉi retejon...', # MODIF
-	'texte_referencement_automatique' => '<b>Aŭtomata retej-referenciga procezo</b><br />Vi povas rapide referencigi retejon indikante ĉi-sube la deziratan retadreson aŭ ties abonligon. SPIP aŭtomate abonŝutos la informojn kiuj koncernas tiun retejon (titolon, priskribon...).', # MODIF
+	'texte_non_fonction_referencement' => 'Vi povas preferi ne uzi tiun aŭtomatan funkcion kaj indiki mem la elementojn koncernante tiun ĉi retejon...',
+	'texte_referencement_automatique' => '<b>Aŭtomata retej-referenciga procezo</b><br />Vi povas rapide referencigi retejon indikante ĉi-sube la deziratan retadreson aŭ ties abonligon. SPIP aŭtomate abonŝutos la informojn kiuj koncernas tiun retejon (titolon, priskribon...).',
+	'texte_referencement_automatique_verifier' => 'Bonvolu kontroli la informojn provitajn de <tt>@url@</tt> antaŭ ol savi.',
 	'texte_syndication' => 'Eblas elpreni aŭtomate la liston de novaĵoj de iu retejo,
   se tiu retejo tion permesas. Por tio, vi devas aktivigi la abonrilaton.
   <blockquote><i>Iuj retgastigantoj malaktivigas tiun funkcion ;
-  tiukaze vi ne povas abonligi la enhavon
-  ekde via retejo.</i></blockquote>', # MODIF
+  tiuokaze, vi ne povas abonligi la enhavon
+  ekde via retejo.</i></blockquote>',
 	'titre_articles_syndiques' => 'Artikoloj abonŝutitaj el tiu retejo',
 	'titre_dernier_article_syndique' => 'Laste abonŝutitaj artikoloj',
+	'titre_exporter_bookmarks' => 'Eksporti paĝosignojn',
+	'titre_importer_bookmarks' => 'Importi paĝosignojn',
+	'titre_importer_exporter_bookmarks' => 'Importi kaj eksporti paĝosignojn',
 	'titre_page_sites_tous' => 'La referencigitaj retejoj',
 	'titre_referencement_sites' => 'Referencigado de retejoj kaj abonrilato',
 	'titre_site_numero' => 'RETEJO NUMERO :',
@@ -113,5 +157,9 @@ ligilojn al jena aŭ alia retejo.', # MODIF
 	'titre_sites_references_rubrique' => 'La retejoj referencigitaj en tiu ĉi rubriko',
 	'titre_sites_syndiques' => 'La abonligitaj retejoj',
 	'titre_sites_tous' => 'La referencigitaj retejoj',
-	'titre_syndication' => 'Reteja abonligo'
+	'titre_syndication' => 'Reteja abonligo',
+	'tout_voir' => 'Vidu ĉiujn abonligitajn artikolojn',
+
+	// U
+	'un_article_syndic' => '1 abonligita artikolo'
 );
