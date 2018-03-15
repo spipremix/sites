0576f99dbf2f4ec6b44b881b42a3fec0b3a24b68
gpl <gpl@long-player.net>
2018-03-15 04:42:13 +0000

    [Salvatore] [source:_core_/plugins/sites/lang/ paquet-sites] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-sites_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-sites?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'sites_description' => 'Retejoj kaj abonrilato en SPIP (privata kaj publika)',
+	'sites_slogan' => 'Mastrumado de retejoj kaj de abonrilato en SPIP'
+);
