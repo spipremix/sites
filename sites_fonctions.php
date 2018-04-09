a8e1daf92206ebb4b655eb7d4b3a500d96c60568
Cerdic <cedric@yterium.com>
2018-04-09 15:44:40 +0000

    La balise #RAW_DATA renvoie un tableau de donnees mise en forme par le fonction syndic_article_raw_data_to_array() si la methode de syndication est connue et fourni une fonction de decodage #RAW_DATA* renvoie le champ raw_data brut, au format texte #RAW_DATA{x} renvoie #RAW_DATA|table_valeur{x} #RAW_DATA{a/b} renvoie #RAW_DATA|table_valeur{a/b}

--- sites_fonctions.php
+++ sites_fonctions.php
@@ -56,3 +56,44 @@ function syndic_article_raw_data_to_array($methode_syndication, $raw_data, $raw_
 
 	return $data;
 }
+
+/**
+ * Compile la balise `#RAW_DATA` retournant le champ `raw_data`
+ *
+ * Utile dans une boucle SYNDIC_ARTICLES pour retourner les donnees brutes de syndication.
+ *
+ * @balise
+ * @see table_valeur()
+ * @example
+ *     ```
+ *     #RAW_DATA* renvoie le champ raw_data brut, au format texte
+ *     #RAW_DATA renvoie le champ raw_data au format tableau structure si il a pu etre decode par la fonction fournie par la methode de syndication
+ *     #RAW_DATA{x} renvoie #RAW_DATA|table_valeur{x},
+ *     #RAW_DATA{a/b} renvoie #RAW_DATA|table_valeur{a/b}
+ *     ```
+ *
+ * @param Champ $p
+ *     Pile au niveau de la balise
+ * @return Champ
+ *     Pile complétée par le code à générer
+ **/
+function balise_RAW_DATA_dist($p) {
+	$b = $p->nom_boucle ? $p->nom_boucle : $p->id_boucle;
+	$_raw_data = index_pile($p->id_boucle, 'raw_data', $p->boucles, $b);
+
+	if ($p->etoile) {
+		$p->code = $_raw_data;
+	}
+	else {
+		$_raw_format = index_pile($p->id_boucle, 'raw_format', $p->boucles, $b);
+		$_raw_methode = index_pile($p->id_boucle, 'raw_methode', $p->boucles, $b);
+
+		$p->code = "syndic_article_raw_data_to_array($_raw_methode, $_raw_data, $_raw_format)";
+		if (($v = interprete_argument_balise(1, $p)) !== null) {
+			$p->code = 'table_valeur(' . $p->code . ', ' . $v . ')';
+		}
+	}
+	$p->interdire_scripts = true;
+
+	return $p;
+}
